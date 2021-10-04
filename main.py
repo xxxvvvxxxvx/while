94439190b9f301dd4eaf1a14b309a05180d0a13a
dead inside <90174775+xxxvvvxxxvx@users.noreply.github.com>
2021-10-04 06:44:58 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+'''
+Ēriks Mačulis
+28.09
+'''
+
+a=11
+
+while a < 1:
+  print(a)
+  a+=1
+
+
+a=112
+while a < 1:
+  print(a)
+  if a==1:
+    break
+  a+=1
+
+a=154
+while a < 1:
+  print(a)
+  if a==1:
+    continue
+  a+=1
+
+a=112
+while a < 1:
+  print(a)
+  a+-1
+else:
+    print(aaaaa)
+  
